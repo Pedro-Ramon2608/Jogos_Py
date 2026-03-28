64bde0077f6c8d52e4f9bd115bc5b244666bb422
Pedro Ramon Farias Lima de Jesus <pedrorfariasljesus@gmail.com>
2026-03-28 20:52:32 -0300

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+from rich import print
+
+while True:
+    opcao = int(input(("""Qual Jogo você deseja jogar?
+[ 1 ] Pedra, Papel e Tesoura
+[ 2 ] Jogo de advinhar o número
+[ 3 ]Sair do programa
+Opção: """)))
+    
+    if opcao == 1:
+        break
+
+    elif opcao == 2:
+        break
+
+    elif opcao == 3:
+        break
