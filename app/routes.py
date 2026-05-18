b712f5e03739e6099d936a5250426ed0108be466
Pedro Ramon Farias Lima de Jesus <pedrorfariasljesus@gmail.com>
2026-05-17 23:53:59 -0300

    concerto em routes na lógica de dados

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, url_for, request
-from logica_jogos import rodar_ppt
+from app.logica_jogos import rodar_ppt
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,10 +16,10 @@ def ped_pap_tes():
 
     # Recebe os dados enviados pelo usuário
     if request.method == "POST":
-        form_usuario = request.form.get("escolha") # 'pedra', 'papel' ou 'tesoura'
+        resposta_usuario = request.form.get("escolha") # 'pedra', 'papel' ou 'tesoura'
 
-    # Chamada da função do pedra, papel e tesoura, do arquivo logica_jogos.py
-    escolha_usuario, escolha_computador, resultado = rodar_ppt(form_usuario) 
+        # Chamada da função do pedra, papel e tesoura, do arquivo logica_jogos.py
+        escolha_usuario, escolha_computador, resultado = rodar_ppt(resposta_usuario) 
 
     return render_template("ped_pap_tes.html", usuario=escolha_usuario, computador=escolha_computador, resultado=resultado)
 
